8e79ccd3d22feef33e1d04448243ec5ce33519e0
poppy <zhangmanazhuang@126.com>
2016-12-06 18:07:39 +0800

    poppy modify test framework to support running with maven and tetstng

--- src/main/java/WB2CConstants/NodeIPConstants.java
+++ src/main/java/WB2CConstants/NodeIPConstants.java
@@ -2,5 +2,7 @@ package WB2CConstants;
 
 public class NodeIPConstants {
 	public static String linux_Node1_ip = "http://10.0.245.111:5555";
+	public static String windows_Node1_ip = "http://10.0.70.56:6666";
+	public static String windows_local_ip = "http://10.0.70.56:5555";
 
 }
