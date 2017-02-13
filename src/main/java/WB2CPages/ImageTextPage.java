f145e4d732a6f9d096fbf211dec763a07e33aa33
poppy <Poppy_Zhang@epam.com>
2017-02-13 18:10:00 +0800

    modify image text create function

--- src/main/java/WB2CPages/ImageTextPage.java
+++ src/main/java/WB2CPages/ImageTextPage.java
@@ -3,8 +3,13 @@ package WB2CPages;
 import WB2CCommon.CommonWebDriver;
 import WB2CConstants.SideMenuConstants;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
+
+import java.awt.*;
 
 
 public class ImageTextPage {
@@ -13,6 +18,7 @@ public class ImageTextPage {
         String testDataPath = System.getProperty("testdata");
         CommonWebDriver.clickElementWhenPresent(driver,
                 By.xpath(SideMenuConstants.wechat_management_xpath));
+        CommonWebDriver.wait(driver, 2);
         CommonWebDriver.clickElementWhenPresent(driver, By.xpath(SideMenuConstants.wechat_text_image_xpath));
         CommonWebDriver.wait(driver, 4);
         CommonWebDriver.switchToFrame(driver, By.xpath("//iframe[@id='materialNewsMgmtFrame']"));
@@ -20,8 +26,12 @@ public class ImageTextPage {
         CommonWebDriver.wait(driver, 2);
         CommonWebDriver.sendKeysToElement(driver, By.xpath("//input[@id='title']"), "test title", 1);
         CommonWebDriver.sendKeysToElement(driver, By.xpath("//input[@id='author']"), "test author", 1);
-        driver.findElement(By.xpath("//input[@type='file']")).sendKeys(testDataPath);
         CommonWebDriver.wait(driver, 2);
+        JavascriptExecutor removeAttribute = (JavascriptExecutor)driver;
+        removeAttribute.executeScript("document.getElementsByClassName('k-widget k-upload k-header k-upload-empty')[0].style.display='block';", new Object[0]);
+        CommonWebDriver.wait(driver, 3);
+        driver.findElement(By.xpath("//input[@type='file']")).sendKeys(testDataPath);
+        CommonWebDriver.wait(driver, 3);
         CommonWebDriver.sendKeysToElement(driver, By.xpath("//textarea[@id='summary']"), "test summary", 1);
         CommonWebDriver.sendKeysToElement(driver, By.xpath("//input[@id='url']"), "https://www.baidu.com", 1);
         CommonWebDriver.clickElement(driver, By.xpath("//div[@id='tabs']/ul/li[2]/a"));
