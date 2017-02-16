6aadc2856fda421a8c3e4b4b9096f6f38c1dd1f5
poppy <Poppy_Zhang@epam.com>
2017-02-16 17:09:18 +0800

    modify a common file

--- src/main/java/WB2CConstants/NodeIPConstants.java
+++ src/main/java/WB2CConstants/NodeIPConstants.java
@@ -1,7 +1,6 @@
 package WB2CConstants;
 
 public class NodeIPConstants {
-    public static String linux_Node1_ip = "http://10.0.245.111:5555";
     public static String windows_Node1_ip = "http://10.0.245.81:5555";
     public static String windows_local_ip = "http://10.0.70.57:5555";
 }
