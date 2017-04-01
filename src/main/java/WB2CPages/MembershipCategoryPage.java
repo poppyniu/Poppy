c05f080a94b16995325dd72cbe9de1a531cd496a
poppy <Poppy_Zhang@epam.com>
2017-04-01 14:29:13 +0800

    modify membership category page file

--- src/main/java/WB2CPages/MembershipCategoryPage.java
+++ src/main/java/WB2CPages/MembershipCategoryPage.java
@@ -5,11 +5,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class MembershipCategoryPage {
-    public static void CreateMembershipCategory(WebDriver driver) throws Exception {
+    public static void CreateMembershipCategory(WebDriver driver,String categoryName) throws Exception {
         CommonWebDriver.clickElement(driver, By.xpath("//a[@id='btnNew']"));
         CommonWebDriver.wait(driver, 2);
-        String expectValue = "test category";
-        CommonWebDriver.sendKeysToElement(driver, By.xpath("//input[@id='name']"), expectValue);
+        CommonWebDriver.sendKeysToElement(driver, By.xpath("//input[@id='name']"), categoryName);
         CommonWebDriver.sendKeysToElement(driver, By.xpath("//input[@id='description']"), "test description");
         CommonWebDriver.clickElement(driver, By.xpath("//a[@id='btnSave']"));
         CommonWebDriver.wait(driver, 2);
